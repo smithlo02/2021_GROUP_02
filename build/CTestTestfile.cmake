fb21045365fd229a303be5a51486ec6931ca7aab
Louis Smith <eeyls6@nottingham.ac.uk>
2022-04-05 14:13:22 +0100

    fixing .yml

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/louis/Documents/university/2021_GROUP_02
-# Build directory: C:/Users/louis/Documents/university/2021_GROUP_02/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(libraryTest "libraryTest")
-set_tests_properties(libraryTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/louis/Documents/university/2021_GROUP_02/CMakeLists.txt;80;add_test;C:/Users/louis/Documents/university/2021_GROUP_02/CMakeLists.txt;0;")

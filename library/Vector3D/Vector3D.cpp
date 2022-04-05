f1ecbf62cd450d96f4b4d12765dfb32e1ebbc7aa
Louis Smith <eeyls6@nottingham.ac.uk>
2022-04-05 16:28:11 +0100

    Setting CTest to only library

--- library/Vector3D/Vector3D.cpp
+++ library/Vector3D/Vector3D.cpp
@@ -3,7 +3,7 @@
 // Copyright Shiyu Wang(20158146)
 // Include header file for Vector3D
 //-------------------------------------------------------------
-#include <Vector3d.h>
+#include <Vector3D.h>
 //---------------------------------------------------------------------------------------------------------------
 //default constructor
 vector3D ::vector3D()

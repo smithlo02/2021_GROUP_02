df5a5520ff29c28a1163c1a1f996119a076beb4f
Louis Smith <eeyls6@nottingham.ac.uk>
2022-04-05 15:23:14 +0100

    Setting CTest to only library

--- library/model/model.cpp
+++ library/model/model.cpp
@@ -24,7 +24,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "..\vector3D\vector3D.h"
+#include "..\Vector3D\Vector3D.h"
 #include "..\cell\cell.h"
 #include "..\material\material.h"
 #include "model.h"
